22bbd45f06ac2d1ea4feab10c67f1c381b1da246
Usandfriends <usandfriends@yahoo.com>
2014-01-04 18:39:10 -0800

    Update reswin.hpp

--- dist/reswin.hpp
+++ dist/reswin.hpp
@@ -1,13 +1,13 @@
 /*
  *  reswin.hpp
- *  ChessPlusPlus
+ *  SimpleChess
  *
  *  Created by Ronak Gajrawala on 12/01/13.
  *  Copyright (c) 2013 Ronak Gajrawala. All rights reserved.
  */
 
-#ifndef ChessPlusPlus_reswin_hpp
-#define ChessPlusPlus_reswin_hpp
+#ifndef SimpleChess_reswin_hpp
+#define SimpleChess_reswin_hpp
 
 const std::string ResourcePath = "res/"; /**< The resource path has to be set manually by the user in Windows. */
 
